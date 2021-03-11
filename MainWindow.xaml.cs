377ba35c94a737592bc5cc8ce57389e84f94604e
ben <moon791017@gmail.com>
2021-03-11 16:44:01 +0800

    ok

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,8 +1,6 @@
-﻿using ReactiveUI;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reactive.Disposables;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,41 +18,17 @@ namespace WpfApp1
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window, IViewFor<MainWindowViewModel>
+    public partial class MainWindow : Window
     {
-        public static readonly DependencyProperty ViewModelProperty = DependencyProperty
-            .Register(nameof(ViewModel), typeof(MainWindowViewModel), typeof(MainWindow));
+        
 
         public MainWindow()
         {
             InitializeComponent();
-            ViewModel = new MainWindowViewModel();
-
-            // Setup the bindings
-            // Note: We have to use WhenActivated here, since we need to dispose the
-            // bindings on XAML-based platforms, or else the bindings leak memory.
-            this.WhenActivated(disposable =>
-            {
-                this.Bind(this.ViewModel, x => x.TheText, x => x.TheTextBox.Text)
-                    .DisposeWith(disposable);
-                this.OneWayBind(this.ViewModel, x => x.TheText, x => x.TheTextBlock.Text)
-                    .DisposeWith(disposable);
-                this.BindCommand(ViewModel, x => x.TheTextCommand, x => x.TheTextButton)
-                    .DisposeWith(disposable);
-            });
+           
             
         }
 
-        public MainWindowViewModel ViewModel
-        {
-            get => (MainWindowViewModel)GetValue(ViewModelProperty);
-            set => SetValue(ViewModelProperty, value);
-        }
-
-        object IViewFor.ViewModel
-        {
-            get => ViewModel;
-            set => ViewModel = (MainWindowViewModel)value;
-        }
+       
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Reference 
 - https://intellitect.com/getting-started-model-view-viewmodel-mvvm-pattern-using-windows-presentation-framework-wpf/
+- http://radio-idea.blogspot.com/2018/12/wpf-mvvm.html
